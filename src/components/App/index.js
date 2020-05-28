e9a2cca76972123bd9f89ef5b53220a6e178147d
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-28 17:41:47 +0100

    header fix

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,6 +7,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <h2>
+        Muddled over malts? Boggled by barley? Simply set your particulars below
+        and tap the glass for whisky wisdom.
+      </h2>
     </div>
   );
 }
