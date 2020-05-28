e2ad04f2ae287f4209947d52585f9c4c16152414
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-28 17:57:23 +0100

    fact box + basic styles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 
 import Header from '../Header';
+import RandomFact from '../RandomFact';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h2>
+      <h3 class="subhead">
         Muddled over malts? Boggled by barley? Simply set your particulars below
         and tap the glass for whisky wisdom.
-      </h2>
+      </h3>
+      <RandomFact />
     </div>
   );
 }
