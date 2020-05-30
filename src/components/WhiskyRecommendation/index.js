53d12020456a245b4eb3bedb42964925600d04a2
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-30 23:13:52 +0100

    whisky rec comp set up w dummy data and initial styling

--- src/components/WhiskyRecommendation/index.js
+++ src/components/WhiskyRecommendation/index.js
@@ -5,7 +5,18 @@ import css from './whiskyRecommendation.module.css';
 function WhiskyRecommendation() {
   return (
     <div id={css.whiskyRecommendation}>
-      <p>test</p>
+      <h4>Whisky Name</h4>
+      <img
+        id={css.whiskyImg}
+        src="https://www.lumaticimagery.com/img/placeholder-image-sq.png"
+        alt=""
+      />
+      <h4 id={css.region}>Region: region</h4>
+      <h4 id={css.priceRange}>Price range: ££</h4>
+      <h4 id={css.tastingNotesHeader}>Tasting notes include:</h4>
+      <p id={css.tastingNotesList}>
+        lorem, ipsum, lorem, ipsum, lorem, ipsum, lorem, ipsum, lorem
+      </p>
     </div>
   );
 }
