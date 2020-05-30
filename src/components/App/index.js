8837ecade1da287195b66ab16e10b7202cf6afbe
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-30 22:49:14 +0100

    reducer structure set up to control fetchCriteriaState

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import './App.css';
 
 import Header from '../Header';
@@ -6,10 +6,41 @@ import Dropdowns from '../Dropdowns';
 import GlassButton from '../glassButton';
 import RandomFact from '../RandomFact';
 
+import { ADD_REGION, ADD_PRICE_RANGE, ADD_FLAVOUR_MOOD } from './actionTypes';
+
 //TODO: need a screen for if the selection doesn't result in anything (i.e. they select specifics from all 3 categories and there isn't a match that meets the 3 characteristics); can have a tip to start broader, maybe with region and price or just flavor
 
+const apiUrl = 'https://evening-citadel-85778.herokuapp.com/';
+
+//initial state for fetchCriteria reducer
+const initialFetchCriteriaState = 'shoot/?';
+
+export function fetchCriteriaReducer(fetchCriteriaState, action) {
+  const { type } = action;
+  switch (type) {
+    case ADD_REGION:
+      //add region=x&
+      return;
+    case ADD_PRICE_RANGE:
+      //add price=x&
+      return;
+    case ADD_FLAVOUR_MOOD:
+      //add
+      return;
+    default:
+      return fetchCriteriaState;
+  }
+}
+
 function App() {
+  //state that manages whether the initial screen w/ dropdowns shows or the results screen:
   const [showWhisky, setShowWhisky] = useState(true);
+
+  //reducer that populates fetch for whisky matching criteria:
+  const [fetchCriteriaState, dispatch] = useReducer(
+    fetchCriteriaReducer,
+    initialFetchCriteriaState
+  );
 
   return (
     <div className="App">
