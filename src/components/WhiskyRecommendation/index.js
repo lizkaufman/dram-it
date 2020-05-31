2d78d57a2af6f72fb221d351f5879bbb19295611
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-31 23:39:17 +0100

    name and region hooked up; price range and tasting notes set up w/ states; need functions to get them out of the object

--- src/components/WhiskyRecommendation/index.js
+++ src/components/WhiskyRecommendation/index.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import css from './whiskyRecommendation.module.css';
 
 function WhiskyRecommendation({ whiskyResult }) {
+  //state to hold priceRange in £ (comes from API in $):
+  const [poundsPriceRange, setPoundsPriceRange] = useState('test');
+  //state to hold tag array:
+  const [flavours, setFlavours] = useState(['test', 'test2', 'test3']);
+
+  useEffect(() => {
+    //TODO: convert $ in whiskyResult to £
+  }, [whiskyResult]);
+
   return (
     <div id={css.whiskyRecommendation}>
-      <h4 className={css.infoSection}>Whisky Name</h4>
+      <h4 className={css.infoSection}>{whiskyResult.title}</h4>
       <img
         id={css.whiskyImg}
         src="https://www.lumaticimagery.com/img/placeholder-image-sq.png"
         alt=""
       />
       <h4 className={css.infoSection} id={css.region}>
-        Region: region
+        Region: {whiskyResult.region}
       </h4>
       <h4 className={css.infoSection} id={css.priceRange}>
-        Price range: ££
+        Price range: {poundsPriceRange}
       </h4>
       <div className={css.infoSection}>
         <h4 id={css.tastingNotesHeader}>Tasting notes include:</h4>
         <p id={css.tastingNotesList}>
-          lorem, ipsum, lorem, ipsum, lorem, ipsum, lorem, ipsum, lorem
+          {flavours.map((flavour) => (
+            <span className={css.flavour} key={flavour}>
+              flavour{' '}
+            </span>
+          ))}
         </p>
       </div>
     </div>
