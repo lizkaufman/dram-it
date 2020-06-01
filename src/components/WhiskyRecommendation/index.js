c654b06146810e7cd876efa496d9b8f403b52570
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-06-02 00:45:15 +0100

    nearly there - correct message pops up for a sec then errors

--- src/components/WhiskyRecommendation/index.js
+++ src/components/WhiskyRecommendation/index.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react';
 
 import css from './whiskyRecommendation.module.css';
 
-function WhiskyRecommendation({ whiskyResult, price, tags }) {
+import NoRecTryAgain from './NoRecTryAgain';
+
+function WhiskyRecommendation({ whiskyResult, price, tags, handleTryAgain }) {
   //state to hold priceRange in £ (comes from API in $):
   const [poundsPriceRange, setPoundsPriceRange] = useState('');
-  //state to hold tag array:
 
   useEffect(() => {
     //convert priceRange from $ to £ for display:
@@ -19,33 +20,39 @@ function WhiskyRecommendation({ whiskyResult, price, tags }) {
   }, [whiskyResult]);
 
   return (
-    <div className={css.whiskyRecommendation}>
-      <h4 className={css.infoSection}>{whiskyResult.title}</h4>
-      <img id={css.whiskyImg} src={whiskyResult.detail_img_url} alt="" />
-      <h4 className={css.infoSection} id={css.region}>
-        Region: {whiskyResult.region}
-      </h4>
-      <h4 className={css.infoSection} id={css.priceRange}>
-        Price range: {poundsPriceRange}
-      </h4>
-      <div className={css.infoSection}>
-        <h4 id={css.tastingNotesHeader}>Tasting notes include:</h4>
-        <p id={css.tastingNotesList}>
-          {tags.map((flavour, i) =>
-            i < tags.length - 1 ? (
-              <span className={css.flavour} key={flavour}>
-                {flavour}
-                {', '}
-              </span>
-            ) : (
-              <span className={css.flavour} key={flavour}>
-                {flavour}
-              </span>
-            )
-          )}
-        </p>
-      </div>
-    </div>
+    <>
+      {whiskyResult ? (
+        <div className={css.whiskyRecommendation}>
+          <h4 className={css.infoSection}>{whiskyResult.title}</h4>
+          <img id={css.whiskyImg} src={whiskyResult.detail_img_url} alt="" />
+          <h4 className={css.infoSection} id={css.region}>
+            Region: {whiskyResult.region}
+          </h4>
+          <h4 className={css.infoSection} id={css.priceRange}>
+            Price range: {poundsPriceRange}
+          </h4>
+          <div className={css.infoSection}>
+            <h4 id={css.tastingNotesHeader}>Tasting notes include:</h4>
+            <p id={css.tastingNotesList}>
+              {tags.map((flavour, i) =>
+                i < tags.length - 1 ? (
+                  <span className={css.flavour} key={flavour}>
+                    {flavour}
+                    {', '}
+                  </span>
+                ) : (
+                  <span className={css.flavour} key={flavour}>
+                    {flavour}
+                  </span>
+                )
+              )}
+            </p>
+          </div>
+        </div>
+      ) : (
+        <NoRecTryAgain handleTryAgain={handleTryAgain} />
+      )}
+    </>
   );
 }
 
