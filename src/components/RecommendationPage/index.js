8e31b570df84a8035c984a8136751ef3c9a55de3
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-06-09 23:20:45 +0100

    set up RecommendationPage component... still thinking thru if this is the best way

---
+++ src/components/RecommendationPage/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from '../Header';
+import WhiskyRecommendation from '../WhiskyRecommendation';
+import Footer from '../Footer';
+
+import css from './recommendationPage.module.css';
+
+function RecommendationPage() {
+  return (
+    <div>
+      <Header />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default RecommendationPage;
